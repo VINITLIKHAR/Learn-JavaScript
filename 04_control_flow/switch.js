6495dd45e30ade9444a4edf06b174acee6581c00
unknown <vinitlikhar2004@gamil.com>
2026-09-07 18:40:08 +0530

    life.js | one.js | switch.js | turethy.js

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,54 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log("january");
+        break;
+    case 2:
+        console.log("Feb");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
+
+
+// const month = "march"
+
+// switch (month) {
+//     case "jan":
+//         console.log("january");
+//         break;
+//     case "Feb":
+//         console.log("Feb");
+//         break;
+//     case "March":
+//         console.log("March");
+//         break;
+//     case "April":
+//         console.log("April");
+//         break;
+
+//     default:
+//         console.log("default case match");
+//         break;
+// }
+
+
---
+++ 04_control_flow/turethy.js
@@ -0,0 +1,58 @@
+const UserEmail =  "vinit@.ai"
+
+if (UserEmail) {
+    console.log("Got user email");
+
+}else {
+    console.log("Don't Have user emil");
+    
+
+}
+
+// Falsy values
+
+// false, 
+// 0, 
+// -0, 
+// BigInt 0n, 
+// "", 
+// null,
+//  undefined
+//  ,NaN
+
+
+// truthy value 
+
+// "0", `false`, " ", function () {}
+
+// if (UserEmail.length === 0) {
+//     console.log("Array id empty");
+    
+// }
+
+
+const emptyObj = {}
+
+if (Object.keys (emptyObj).length === 0) {
+    console.log("Object is empty");
+    
+}
+
+
+// Nullish Coalescing Operator (??) : Null undefined
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 15
+val1 = null ?? 10 ?? 20
+
+
+console.log(val1);
+
+// Terniary  Operator
+
+// Condition ? true : false 
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more then 80");
